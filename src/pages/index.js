ba818e957055243e8c2f05a43220d813c19bcc27
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-31 14:13:59 +0600

    project complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
+import { Avatar } from "antd";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 const HomePage = () => {
   const { data: session } = useSession();
+
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
       <h1 style={{ textAlign: "center", marginTop: "10%" }}>Welcome To Next Auth Home Page</h1>
-      <h1>{session?.user?.name}</h1>
-      <h1>{session?.user?.email}</h1>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Avatar size={96} src={session?.user?.image} />
+      </div>
+      <h1 style={{ textAlign: "center" }}>{session?.user?.name}</h1>
+      <h1 style={{ textAlign: "center" }}>{session?.user?.email}</h1>
     </div>
   );
 };
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,14 +2,18 @@ import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
 import Head from "next/head";
+import { useSession } from "next-auth/react";
 const ProfilePage = () => {
+  const { data: session } = useSession();
   return (
     <div style={{ textAlign: "center" }}>
       <Head>
         <title>Next Profile</title>
       </Head>
       <h1>User Profile</h1>
-      <Avatar size={64} icon={<UserOutlined />} />
+      <Avatar size={96} src={session?.user?.image} />
+
+
     </div>
   );
 };
